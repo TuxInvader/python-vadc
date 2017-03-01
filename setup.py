3ff9962c7f6b3286414777259424c8ea29c4e58a
Mark Boddington <tuxinvader@users.noreply.github.com>
2017-03-01 12:35:21 +0000

    version 0.0.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.6',
+    version='0.0.7',
 
     description='A python library for vADC',
     long_description=long_description,
